a0a300e17c278fbf9c5b88476173e660fbed69e3
Vladislav <yudin471@gmail.com>
2019-01-03 23:10:41 +0200

    5

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -21,12 +21,8 @@ if ('Notification' in window) {
     });
 }
 
-function subscribe() {
-    // запрашиваем разрешение на получение уведомлений
-    messaging.requestPermission()
-        .then(function () {
-            // получаем ID устройства
-            messaging.getToken()
+function get_token(){
+    messaging.getToken()
                 .then(function (currentToken) {
                     console.log(currentToken);
 
@@ -41,37 +37,4 @@ function subscribe() {
                     console.warn('При получении токена произошла ошибка.', err);
                     setTokenSentToServer(false);
                 });
-    })
-    .catch(function (err) {
-        console.warn('Не удалось получить разрешение на показ уведомлений.', err);
-    });
-}
-
-// отправка ID на сервер
-function sendTokenToServer(currentToken) {
-    if (!isTokenSentToServer(currentToken)) {
-        console.log('Отправка токена на сервер...');
-
-        var url = ''; // адрес скрипта на сервере который сохраняет ID устройства
-        $.post(url, {
-            token: currentToken
-        });
-
-        setTokenSentToServer(currentToken);
-    } else {
-        console.log('Токен уже отправлен на сервер.');
-    }
-}
-
-// используем localStorage для отметки того,
-// что пользователь уже подписался на уведомления
-function isTokenSentToServer(currentToken) {
-    return window.localStorage.getItem('sentFirebaseMessagingToken') == currentToken;
-}
-
-function setTokenSentToServer(currentToken) {
-    window.localStorage.setItem(
-        'sentFirebaseMessagingToken',
-        currentToken ? currentToken : ''
-    );
 }
